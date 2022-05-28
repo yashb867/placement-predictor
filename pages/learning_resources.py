0a6bb16ddfed2998b0a2720f5d6f803ee0e402f8
yashb-accolite <yash.bhokare@accolitedigital.com>
2022-05-29 00:51:21 +0530

    Update learning_resources.py

--- pages/learning_resources.py
+++ pages/learning_resources.py
@@ -4,23 +4,44 @@ import os, pickle
 
 # @st.cache
 def app():
-    st.markdown("## Please refer to the study material provided below!")
+    st.markdown("## Do refer to the resources provided below!")
     resources = [
         {
-            "Name": "Name of topic",
-            "Description": "Description about topic",
+            "Name": "Resume-building",
+            "Description": "Start building your resume with this cool templates",
+            "link": "https://novoresume.com"
+        },
+        {
+            "Name": "Aptitude",
+            "Description": "Work on your Problem solving skills",
+            "link": "https://www.indiabix.com/"
+        },
+        {
+            "Name": "Courses",
+            "Description": "Attend some courses to get started with your area of interests",
+            "link": "https://www.udemy.com/"
+        },
+        {
+            "Name": "Programming",
+            "Description": "To get started with hands-on coding visit this link",
+            "link": "https://www.hackerrank.com/"
+        },
+        {
+            "Name": "Data Structures and Algorithms",
+            "Description": "Click below link for solving DSA problems",
             "link": "http://paste/link.here"
         },
         {
-            "Name": "Name of topic",
-            "Description": "Description about topic",
-            "link": "http://paste/link.here"
+            "Name": "Brainstorming Puzzles",
+            "Description": "Strain your brain with this puzzles",
+            "link": "https://www.geeksforgeeks.org/puzzles/"
         },
         {
-            "Name": "Name of topic",
-            "Description": "Description about topic",
-            "link": "http://paste/link.here"
-        }
+            "Name": "Scholarships",
+            "Description": "You can avail some scholarships to get started with various exciting technologies",
+            "link": "https://www.udacity.com/scholarships"
+        },
+
     ]
     st.markdown("""---""")
     counter = 1
